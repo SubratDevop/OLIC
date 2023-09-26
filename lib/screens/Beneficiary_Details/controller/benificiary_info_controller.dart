a1aa67f9f39b087f457b28dcf6f89aee3111cf34
SubratDevop <subratDev404@gmail.com>
2023-09-26 11:50:18 +0530

    BeneficiaryInfoScreen  Getx added

--- lib/screens/Beneficiary_Details/controller/benificiary_info_controller.dart
+++ lib/screens/Beneficiary_Details/controller/benificiary_info_controller.dart
@@ -1,8 +1,16 @@
 import 'package:get/get.dart';
 
+import '../model/user_modal.dart';
+
 class BenificiaryInfoController extends GetxController {
+   List<User> users = [];
+ 
+ 
+ 
+ 
   @override
   void onInit() {
     super.onInit();
+    users = User.allUser();
   }
 }
--- lib/screens/Beneficiary_Details/screens/benificiary_info_screen.dart
+++ lib/screens/Beneficiary_Details/screens/benificiary_info_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:olic/commons/color_gallery.dart';
 import 'package:olic/constants/screensize.dart';
@@ -8,27 +7,16 @@ import 'package:olic/widgets/default_appbar.dar.dart';
 
 import '../../../widgets/custm_text.dart';
 import '../../Farmer registration Details/farmer_registration_details_screen.dart';
+import '../controller/benificiary_info_controller.dart';
 import '../model/user_modal.dart';
 
-class BeneficiaryInfoScreen extends StatefulWidget {
+class BeneficiaryInfoScreen extends StatelessWidget {
   int? index;
   BeneficiaryInfoScreen({ this.index});
 
   @override
-  State<BeneficiaryInfoScreen> createState() => _BeneficiaryInfoScreenState();
-}
-
-class _BeneficiaryInfoScreenState extends State<BeneficiaryInfoScreen> {
-  List<User>? users;
-
-  @override
-  void initState() {
-    users = User.allUser();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    BenificiaryInfoController controller = Get.put(BenificiaryInfoController());
     return Scaffold(
       backgroundColor: ColorGallery.white_color,
       appBar: defaultAppBar(context,
@@ -53,14 +41,14 @@ class _BeneficiaryInfoScreenState extends State<BeneficiaryInfoScreen> {
             ),
           
           
-            infoWidget("Registration No", users![widget.index!].regiStrationNo),
-            infoWidget("Name", users![widget.index!].name),
-            infoWidget("Village", users![widget.index!].village),
-            infoWidget("Gramapanchayat", users![widget.index!].gramaPanchayat),
-            infoWidget("Block", users![widget.index!].block),
-            infoWidget("District", users![widget.index!].district),
-            infoWidget("Details View", users![widget.index!].detailsView),
-            infoWidget("Action", users![widget.index!].action),
+            infoWidget("Registration No", controller.users[index!].regiStrationNo),
+            infoWidget("Name", controller.users[index!].name),
+            infoWidget("Village", controller.users[index!].village),
+            infoWidget("Gramapanchayat", controller.users[index!].gramaPanchayat),
+            infoWidget("Block", controller.users[index!].block),
+            infoWidget("District", controller.users[index!].district),
+            infoWidget("Details View", controller.users[index!].detailsView),
+            infoWidget("Action", controller.users[index!].action),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 5.0),
               child: Divider(
@@ -94,7 +82,7 @@ class _BeneficiaryInfoScreenState extends State<BeneficiaryInfoScreen> {
               ),
             ),
             SizedBox(
-              width: ScreenSize.width(context) * 0.45,
+              width: ScreenSize.width(Get.context!) * 0.45,
               child: details == "detailsView"
                   ? Buttons(
                     onTapBtn: () => Get.to(const FarmerRegistrationDetailsScreen()),
@@ -116,9 +104,4 @@ class _BeneficiaryInfoScreenState extends State<BeneficiaryInfoScreen> {
       ],
     );
   }
-
-
-
-
-
 }
